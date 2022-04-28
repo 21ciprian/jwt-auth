2325eba1d7f5a9d82da3c4a13f0f92671e63bfd5
21ciprian <2021ciprianoffice@gmail.com>
2022-04-28 22:40:56 +0100

    Imported usersRouter into app

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import cors from 'cors'
 import express from 'express'
+import usersRouter from './routes/usersRouter.js'
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 const PORT = process.env.PORT || 5000
-
+app.use('/api', usersRouter)
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
